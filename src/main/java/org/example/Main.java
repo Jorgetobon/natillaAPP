1ca0aa27eda13f66d0c3d048ab9ab1fdaac20d27
Esteban129 <danilobenitez14m@gmail.com>
2023-12-04 15:35:10 -0500

    se crea proyecto

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Entradas del problema
+        String nombreUsuario="Jorge Mario Tobon Ardila";
+        short edadUsuario=23;
+        String telefonoUsuario="312678890";
+             String documentoIdentidad="13289793449";
+             int cuotaEnero;
+             int cuotaFebrero;
+             int cuotaMarzo;
+             int cuotaAbril;
+             int cuotaMayo;
+             int cuotaJunio;
+             int cuotaJulio;
+             int cuotaAgosto;
+             int cuotaSeptiembre;
+             int cuotaOctubre;
+             int cuotaNoviembre;
+             String direccionUsuario="CLL 39# 80-20";
+             double costoInscripcion=150000.0;
+             float tasaInteresNatillera=0.1F;
+             double saldoFinalUsuario;
+
+             //PROCESO Para procesar Datos
+             // desde el TECLADO (Recibir)
+        Scanner leerDato = new Scanner(System.in);
+
+        System.out.println("Hola soy una IA, cual es tu nombre");
+        //leerDato.next();
+
+
+
+
+
+    }
+}
